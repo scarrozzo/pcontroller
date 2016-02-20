9c4bf7a5c27adc487470bb05bc925fcd0511072b
scarrozzo <sergio.carrozzo@gmail.com>
2016-02-20 15:11:17 +0100

    Fix main procedure

--- Code/pcontroller_main.py
+++ Code/pcontroller_main.py
@@ -11,24 +11,28 @@ jobs = {}
 
 # general purpose function for modules
 def module_runner(module):
-	result = sys.modules[module].run();
-	gitConnection.store_module_result(result, pcontroller_id, "Auto commit message", module);
-	del jobs[module];
-	return 
+	try:
+		result = sys.modules[module].run();
+		gitConnection.store_module_result(result, pcontroller_id, "Auto commit message", module);
+	finally:
+		del jobs[module];
+		return 
 
+def get_GitConnection():
+	gitConnection = Git();
+	gitConnection.import_modules(pcontroller_constants.PCONFIG);
+	return gitConnection;
 
 #main
 sys.meta_path = [GitImporter()];
 
-gitConnection = Git();
-gitConnection.import_modules(pcontroller_constants.PCONFIG);
-cmd = gitConnection.get_cmd(pcontroller_constants.PCOMMANDS);
-
 while True:
+	gitConnection = get_GitConnection();
 	cmd = gitConnection.get_cmd(pcontroller_constants.PCOMMANDS);
+	print cmd
 	for command in cmd:
 		if "module" in command and "when" in command:
 			if not command["module"] in jobs:
 				jobs[command["module"]] = True;
 				threading.Timer(int(command["when"]), module_runner, [command['module']]).start();
-	time.sleep(1);
+	time.sleep(5);
